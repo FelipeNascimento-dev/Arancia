1ee6e5b95c01012f1450a7430cdb5e9843d74526
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-05-29 12:26:11 -0300

    feat: pagination

--- arancia/views.py
+++ arancia/views.py
@@ -87,10 +87,12 @@ def estorno_rom(request):
 def consulta_id(request):
     form = ConsultaForm()
     tabela_dados = None
+    exibir_formulario = True
 
     if request.method == 'POST':
         form = ConsultaForm(request.POST)
         if form.is_valid():
+            exibir_formulario = False
             tabela_dados = [
                 {"matnr": "000000000000604825",
                 "gernr": "000000000005305833",
@@ -103,13 +105,14 @@ def consulta_id(request):
                 # "nr_lcr_mast": None,
                 # "nr_cd_br_cx": None,
                 "stat_div_rec": "00",
-                # "id_lote": "030000431485",
+                "id_lote": "030000431485",
                 }
             ]
 
     context = {
         'form': form,
         'tabela_dados': tabela_dados,
+        'exibir_formulario': exibir_formulario
     }
     return render(request, 'arancia/consulta_id.html', context)
 
