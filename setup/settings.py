78836b318a93d7ea13e6f016b0276ec72ab6c8f9
IgorSantRocha <igor77876@gmail.com>
2025-06-12 16:38:25 -0300

    Adicionada api na consulta ID

--- setup/settings.py
+++ setup/settings.py
@@ -123,3 +123,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
+
+
+try:
+    from setup.local_settings import *
+except ImportError:
+    ...
