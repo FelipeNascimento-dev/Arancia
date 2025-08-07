8437493d0179a66be45ef8e72634345d2bc7ddea
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-08-07 11:15:14 -0300

    feat: API na reserva de equipamento

--- logistica/forms/forms_reserva_equip.py
+++ logistica/forms/forms_reserva_equip.py
@@ -2,8 +2,8 @@ from django import forms
 
 class ReservaEquipamentosForm(forms.Form):
     serial = forms.CharField(label='Serial', max_length=50)
-    centro = forms.ChoiceField(label='Centro de Custos', choices=[(1, ''), (2, 'WIN')])
-    deposito = forms.ChoiceField(label='Deposito', choices=[(1, ''), (2, '989A')])
+    centro = forms.ChoiceField(label='Centro de Custos', choices=[(1, ''), ('WIN', 'WIN')])
+    deposito = forms.ChoiceField(label='Deposito', choices=[(1, ''), ('989A', '989A')])
 
     def __init__(self, *args, nome_form=None, **kwargs):
         self.nome_formulario = nome_form or "Definir nome do formulário"
