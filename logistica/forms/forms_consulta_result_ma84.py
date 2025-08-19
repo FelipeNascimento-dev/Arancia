ce9fde7cca2c8bc127f3bd8398a26817b97375c4
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-08-19 11:29:05 -0300

    feat: campo de pedido na tela de consulta ma84

--- logistica/forms/forms_consulta_result_ma84.py
+++ logistica/forms/forms_consulta_result_ma84.py
@@ -2,6 +2,7 @@ from django import forms
 
 class ConsultaResultMA84Form(forms.Form):
     nome_formulario = 'SAP - Consulta Resultados MA'
+    pedido = forms.CharField(label='Pedido', max_length=50, required=False)
     tp_reg = forms.ChoiceField(label='Tipo de Registro', choices=[
         ('', ''), 
         ('84', 'Reserva de Equipamento'), 
