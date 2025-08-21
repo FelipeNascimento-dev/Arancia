c7330fc145069a01831fffc379298dbdc684a460
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-08-21 09:23:19 -0300

    feat: ajuste na mensagem de login

--- logistica/views/view_base.py
+++ logistica/views/view_base.py
@@ -4,11 +4,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='logistica:login')
 def index(request):
-    user = request.user
-    first = user.first_name or user.username
-    last = user.last_name or ""
-    messages.info(
-        request,
-        f"👋 Olá, {first} {last}! Seja bem-vindo ao Arancia, nossa nova plataforma de gestão."
-    )
     return render(request, 'global/base.html')
