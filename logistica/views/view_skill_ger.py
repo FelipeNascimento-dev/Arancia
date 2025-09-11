b16c34a02da3bcaae0b7b12dbe202709f081cb7d
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-09-11 16:13:37 -0300

    feat: skill ger editando grupos

--- logistica/views/view_skill_ger.py
+++ logistica/views/view_skill_ger.py
@@ -23,7 +23,7 @@ def skill_ger(request):
             sales_channel=sales_channel,
         )
         messages.success(request, f"Grupo {grupo.nome} criado com sucesso!")
-        return redirect("logistica:group_additional_manager")
+        return redirect("logistica:skill_ger")
 
     group_id = request.GET.get("group_id")
     if group_id:
@@ -32,15 +32,30 @@ def skill_ger(request):
         usuarios_vinculados = User.objects.filter(
             designacao__informacao_adicional=selected_group)
 
-    if request.method == "POST" and "add_user" in request.POST and selected_group:
-        user_id = request.POST.get("user_id")
-        user = get_object_or_404(User, id=user_id)
-        designation, _ = UserDesignation.objects.get_or_create(user=user)
-        designation.informacao_adicional = selected_group
-        designation.save()
+    if request.method == "POST" and "edit_group" in request.POST:
+        group_id = request.POST.get("group_id")
+        grupo = get_object_or_404(GroupAditionalInformation, id=group_id)
+
+        grupo.nome = request.POST.get("nome")
+        grupo.cod_iata = request.POST.get("cod_iata")
+        grupo.sales_channel = request.POST.get("sales_channel")
+        grupo.deposito = request.POST.get("deposito")
+        grupo.logradouro = request.POST.get("logradouro")
+        grupo.numero = request.POST.get("numero")
+        grupo.complemento = request.POST.get("complemento")
+        grupo.bairro = request.POST.get("bairro")
+        grupo.cidade = request.POST.get("cidade")
+        grupo.estado = request.POST.get("estado")
+        grupo.CEP = request.POST.get("cep")
+        grupo.telefone1 = request.POST.get("telefone1")
+        grupo.telefone2 = request.POST.get("telefone2")
+        grupo.email = request.POST.get("email")
+        grupo.responsavel = request.POST.get("responsavel")
+
+        grupo.save()
         messages.success(
-            request, f"Usuário {user.username} vinculado a {selected_group.nome}")
-        return redirect(f"{request.path}?group_id={selected_group.id}")
+            request, f"Grupo {grupo.nome} atualizado com sucesso!")
+        return redirect(f"{request.path}?group_id={grupo.id}")
 
     context = {
         "grupos": grupos,
