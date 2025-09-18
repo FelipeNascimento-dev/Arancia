f1519ed0966bb43c5eb7e01f64f38ae03ceebc85
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-09-18 15:03:28 -0300

    feat: ajuste nos cards

--- logistica/views/view_reverse_create.py
+++ logistica/views/view_reverse_create.py
@@ -6,27 +6,40 @@ def reverse_create(request):
     titulo = 'Reversa de Equipamento'
     form = ReverseCreateForm(request.POST or None, nome_form=titulo)
 
-    if 'seriais' not in request.session:
-        request.session['seriais'] = []
+    if "volumes" not in request.session:
+        request.session["volumes"] = []
+
+    volumes = request.session["volumes"]
 
     if request.method == "POST" and form.is_valid():
         serial = form.cleaned_data.get("serial")
         if serial:
-            seriais = request.session['seriais']
-            seriais.append(serial)
-            request.session['seriais'] = seriais
+            if not volumes:
+                volumes.append({"number": 1, "kits": []})
+
+            ultimo_volume = volumes[-1]
+
+            if len(ultimo_volume["kits"]) >= 10:
+                volumes.append({
+                    "number": len(volumes) + 1,
+                    "kits": []
+                })
+                ultimo_volume = volumes[-1]
+
+            ultimo_volume["kits"].append({
+                "number": len(ultimo_volume["kits"]) + 1,
+                "serial": serial
+            })
+
+            request.session["volumes"] = volumes
             request.session.modified = True
+
         return redirect("logistica:reverse_create")
 
-    todos_seriais = request.session.get('seriais', [])
-    table_reverse = [todos_seriais[i:i+10]
-                     for i in range(0, len(todos_seriais), 10)]
-    table_reverse = list(reversed(table_reverse))
-
     context = {
-        'form': form,
-        'botao_texto': 'Inserir',
-        'site_title': 'Reversa',
-        'table_reverse': table_reverse,
+        "form": form,
+        "botao_texto": "Inserir",
+        "site_title": "Reversa",
+        "volumes": volumes,
     }
-    return render(request, 'logistica/reverse_create.html', context)
+    return render(request, "logistica/reverse_create.html", context)
