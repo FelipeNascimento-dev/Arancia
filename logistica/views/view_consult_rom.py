fe62fdf7671c4f2e3ad6bc9d2c4097e5f9f4a94f
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-09-23 15:17:04 -0300

    feat: API na view de reverse_create

--- logistica/views/view_consult_rom.py
+++ logistica/views/view_consult_rom.py
@@ -1,7 +1,5 @@
-from django.db.models import Max
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from ..models import RomaneioReverse
 from ..forms import RomaneioConsultaForm
 
 
@@ -14,28 +12,10 @@ def consult_rom(request):
         if form.is_valid():
             numero = form.cleaned_data["numero"]
 
-            try:
-                romaneio = RomaneioReverse.objects.get(numero=numero)
-                request.session["romaneio_num"] = romaneio.numero
-                return redirect("logistica:reverse_create")
-
-            except RomaneioReverse.DoesNotExist:
-                ultimo = RomaneioReverse.objects.aggregate(
-                    ultimo=Max("numero"))["ultimo"]
-
-                if ultimo:
-                    proximo_disponivel = str(int(ultimo) + 1).zfill(10)
-                else:
-                    proximo_disponivel = "0000000001"
-
-                messages.warning(
-                    request,
-                    f"O romaneio {numero} não existe. Você pode criar o próximo disponível: {proximo_disponivel}"
-                )
-
-                request.session["romaneio_num"] = proximo_disponivel
+            request.session["romaneio_num"] = numero
+            return redirect("logistica:reverse_create")
     else:
-        form = RomaneioConsultaForm(nome_form=titulo,)
+        form = RomaneioConsultaForm(nome_form=titulo)
 
     return render(request, "logistica/consult_rom.html", {
         "form": form,
