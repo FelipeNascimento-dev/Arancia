a3d13bce485e05deaa6102d5e0378d52d5593c30
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-10-17 12:38:08 -0300

    feat: choices corretos

--- logistica/views/view_order_select.py
+++ logistica/views/view_order_select.py
@@ -1,5 +1,5 @@
 from ..forms import OrderSelectForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def order_select(request):
@@ -7,11 +7,14 @@ def order_select(request):
 
     if request.method == "POST":
         form = OrderSelectForm(request.POST, nome_form=titulo)
+        if form.is_valid():
+            request.session['order'] = form.cleaned_data['order']
+        return redirect('logistica:detalhe_pedido', order=form.cleaned_data['order'])
     else:
         form = OrderSelectForm(request.POST, nome_form=titulo)
 
     return render(request, 'logistica/order_select.html', {
         'form': form,
         'site_title': titulo,
-        'botao_texto': 'Selecionar Ação'
+        'botao_texto': "Selecionar Pedido",
     })
