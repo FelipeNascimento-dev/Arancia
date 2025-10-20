37160036a959df24dbed4032e09a7152a5167136
Daviangelo <daviangelohuene2021@gmail.com>
2025-10-20 18:26:15 -0300

    finally

--- logistica/urls.py
+++ logistica/urls.py
@@ -11,7 +11,8 @@ from .views import index, consulta_id_form, pre_recebimento, \
     consulta_ma84, btn_ma_voltar, consulta_ec01, btn_ec_voltar, consult_rom, \
     logout_confirm_view, logout_view, registrar_usuario, trackingIP, user_ger, skill_ger, \
     extracao_pedidos, consulta_etiquetas, settings_view, reverse_create, delete_btn, \
-    cancel_btn, send_quotes, toggle_db, \
+    cancel_btn, send_quotes, toggle_db, unsuccessful_insert, client_select, client_checkin, \
+    order_select, \
     UserPasswordChangeView
 
 app_name = 'logistica'
@@ -66,4 +67,10 @@ urlpatterns = [
     path('reverse/delete/<str:serial>/', delete_btn, name='delete_btn'),
     path('reverse/cancel/<str:id>/', cancel_btn, name='cancel_btn'),
     path('reverse/envio', send_quotes, name='send_quotes'),
+    path('insucesso/insert/<str:order>/',
+         unsuccessful_insert, name='unsuccessful_insert'),
+    path('check-in/selecao-clientes/', client_select, name='client_select'),
+    path('check-in/order/select/', order_select, name='order_select'),
+    path('check-in/registro/', client_checkin, name='client_checkin'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
