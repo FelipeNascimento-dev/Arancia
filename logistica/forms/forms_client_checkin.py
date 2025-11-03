f4db558709ed93e74316de9b4fba0e999b409757
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2025-11-03 15:16:00 -0300

    feat: origem puxando corretamente

--- logistica/forms/forms_client_checkin.py
+++ logistica/forms/forms_client_checkin.py
@@ -2,23 +2,53 @@ from django import forms
 
 
 class ClientCheckInForm(forms.Form):
-    pedido = forms.CharField(label='Romaneio', max_length=100,
-                             widget=forms.TextInput(attrs={'autocomplete': 'off'}))
-    serial = forms.CharField(label='Serial', max_length=100,
-                             widget=forms.TextInput(attrs={'autocomplete': 'off'}))
-    product = forms.CharField(label='Material', max_length=100,
-                              widget=forms.TextInput(attrs={'autocomplete': 'off'}))
+    pedido = forms.CharField(
+        label='Romaneio',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'off'})
+    )
+    serial = forms.CharField(
+        label='Serial',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'off'})
+    )
+    product = forms.CharField(
+        label='Material',
+        max_length=100,
+        widget=forms.TextInput(attrs={'autocomplete': 'off'})
+    )
     volume = forms.IntegerField(
-        label='Volume (opcional)', min_value=1, required=False)
-    kit = forms.CharField(label='Kit (opcional)',
-                          max_length=100, required=False,)
-    pedido_atrelado = forms.CharField(label='Pedido Atrelado (opcional)',
-                                      max_length=100, required=False,
-                                      widget=forms.TextInput(attrs={'autocomplete': 'off'}))
+        label='Volume (opcional)',
+        min_value=1,
+        required=False
+    )
+    kit = forms.CharField(
+        label='Kit (opcional)',
+        max_length=100,
+        required=False
+    )
+    pedido_atrelado = forms.CharField(
+        label='Pedido Atrelado (opcional)',
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'autocomplete': 'off'})
+    )
     from_location = forms.ChoiceField(
-        label='Origem', required=False, choices=[])
-    to_location = forms.ChoiceField(label='Destino', choices=[])
+        label='Origem',
+        required=False,
+        choices=[]
+    )
+    to_location = forms.ChoiceField(
+        label='Destino',
+        choices=[]
+    )
 
-    def __init__(self, *args, nome_form=None, **kwargs):
+    def __init__(self, *args, **kwargs):
+        from_choices = kwargs.pop('from_choices', None)
+        nome_form = kwargs.pop('nome_form', None)
+
         self.nome_formulario = nome_form or "Definir nome do formulário"
         super().__init__(*args, **kwargs)
+
+        if from_choices:
+            self.fields['from_location'].choices = from_choices
