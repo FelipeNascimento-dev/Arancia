8044fd89b8074f5b6a4488fbab5d2ad773a4027a
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2026-03-20 12:47:10 -0300

    feat: cancelamento de pedido pronto

--- logistica/viewsV2/__init__.py
+++ logistica/viewsV2/__init__.py
@@ -1,6 +1,6 @@
 from .view_tracking_simpl import trackingIPV2
 from .view_consulta_romaneioV2 import consult_romV2
-from .view_criar_reversaV2 import reverse_createV2
+from .view_criar_reversaV2 import reverse_createV2, delete_btnV2, cancel_btnV2
 from .view_enviar_cotacaoV2 import send_quotesV2
 from .view_lista_romaneios import lista_romaneios
 from .view_consulta_cotacao import consulta_cotacao
