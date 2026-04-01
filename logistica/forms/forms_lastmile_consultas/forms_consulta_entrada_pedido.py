b9e6dce7e1b190e6be5115ddaf1ac09fdddb9386
FelipeNascimento-dev <felipe.nasci.dev@gmail.com>
2026-04-01 17:13:20 -0300

    feat: ajustes para subir prod

--- logistica/forms/forms_lastmile_consultas/forms_consulta_entrada_pedido.py
+++ logistica/forms/forms_lastmile_consultas/forms_consulta_entrada_pedido.py
@@ -3,7 +3,7 @@ from django import forms
 
 class OrderConsultForm(forms.Form):
     form_title = 'Consultar Pedido'
-    order = forms.CharField(label='Pedido', max_length=20, required=False)
+    order = forms.CharField(label='Pedido', max_length=50, required=False)
 
     def __init__(self, *args, nome_form=None, **kwargs):
         super().__init__(*args, **kwargs)
