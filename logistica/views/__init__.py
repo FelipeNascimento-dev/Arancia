58c902d0fbd338d44be5acc1eb93f8eea89c6b72
Felipe Nascimento <felipe.nasci.dev@gmail.com>
2026-06-08 17:55:14 -0300

    feat: avanço na tela de pre-recebimento

--- logistica/views/__init__.py
+++ logistica/views/__init__.py
@@ -44,6 +44,6 @@ from .view_gestao_contas import *
 from .view_contas_privilegiadas import gestao_contas_privilegiadas
 from .view_iframe_acompanhamento import acompanhamento_iframe
 from .views_checkin_checkout.view_receber_em_estoque import receber_em_estoque, bipar_serial_recebimento
-from .views_checkin_checkout.view_pre_recebimento import pre_recebimento
+from .views_checkin_checkout.view_pre_recebimento import pre_recebimento_checkin
 from .views_checkin_checkout.view_checkin_bag_tec import checkin_bag_tec
 from .views_checkin_checkout.view_atrelar_motoboy import atrelar_motoboy
